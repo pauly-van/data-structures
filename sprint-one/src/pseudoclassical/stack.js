7412727b31142478662861aeb51f1f871ad7774b
Paul Nguyen <paulngu1120@gmail.com>
2020-09-03 20:46:01 +0000

    completed prototypal and progress on pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
-var Stack = function() {
+var Stack = function(stor, cnt) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = stor;
+  this.counter = cnt;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.counter++] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.counter === 0) {
+    return undefined;
+  }
+  let objKeys = Object.keys(this.storage);
+  let lastElement = this.storage[objKeys.length - 1];
+  delete this.storage[lastElement];
+  this.counter--;
+  return lastElement;
+};
+
+Stack.prototype.size = function() {
+  return this.counter;
+};
+
+var someInstance = new Stack({}, 0);
 
