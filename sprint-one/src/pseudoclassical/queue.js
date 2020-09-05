112cab51f2b253d842bd978d30fe64aeff556a9b
Paul Nguyen <paulnguyen@google.com>
2020-09-05 11:58:49 -0700

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.counter = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.counter++] = value; 
+};
 
+Queue.prototype.dequeue = function() {
+  let resetCnter = 0;
+  if (this.counter === 0) {
+    return undefined;
+  }
+  let objKeys = Object.keys(this.storage);
+  let shiftedElement = this.storage[objKeys[0]];
+  delete this.storage[objKeys[0]];
+  this.counter--;
+  for (let key in this.storage) {
+    this.storage[resetCnter++] = this.storage[key];
+  }
+  return shiftedElement;
+};
+
+Queue.prototype.size = function() {
+  return this.counter;
+};
+
+var someInstance = new Queue;
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,8 +1,8 @@
-var Stack = function(stor, cnt) {
+var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.storage = stor;
-  this.counter = cnt;
+  this.storage = {};
+  this.counter = 0;
 };
 
 Stack.prototype.push = function(value) {
@@ -14,15 +14,15 @@ Stack.prototype.pop = function() {
     return undefined;
   }
   let objKeys = Object.keys(this.storage);
-  let lastElement = this.storage[objKeys.length - 1];
-  delete this.storage[lastElement];
+  let lastElement = objKeys[objKeys.length - 1];
+  let poppedElement = this.storage[lastElement];
   this.counter--;
-  return lastElement;
+  delete this.storage[lastElement];
+  return poppedElement; 
 };
 
 Stack.prototype.size = function() {
   return this.counter;
 };
 
-var someInstance = new Stack({}, 0);
-
+var someInstance = new Stack();
