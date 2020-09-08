598d4aa44050cb6d5220c95988db52dd9c0c5000
Paul Nguyen <paulngu1120@gmail.com>
2020-09-08 22:37:09 +0000

    1st pair session

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,15 +5,38 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     let tailVal = Node(value);
-    if ( list.tail === null ) {
+    if (list.tail !== null) {
+
+      let previousTail = list.tail;
+      previousTail.next = value;
+      list[previousTail.value] = previousTail;
+      list.head["next"] = value;
+      let newVal = Node(value);
+      list.tail = newVal;
+    }
+    if ( list.tail === null && list.head === null)  {
+      list.tail = tailVal;
       list.head = tailVal;
-    }   
-    list.tail = tailVal; 
-    list[tailVal.value] = tailVal; 
+      list[list.tail[value]] = tailVal;
+      
+    } 
+    
+    //list.tail = tailVal;
+ 
+    //console.log(tailVal);
+    
   };
 
   list.removeHead = function() {
-    
+    let currentHead = list.head;
+    console.log(currentHead);
+    console.log(list[list.head.next]);
+    list.head = list[list.head.next]; //need to figure out how to change next
+    let nextHead = list.head.next;
+    console.log(nextHead + 'key');
+    list.head = list[nextHead];
+    delete list[currentHead.value];
+    return currentHead;
   };
 
   list.contains = function(target) {
