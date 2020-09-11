febf99b1542a2d2a2fbb20d14aff202b3a6488b8
Paul Nguyen <paulngu1120@gmail.com>
2020-09-11 19:47:34 +0000

    Complete Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,60 +1,79 @@
-
-
-// Instantiate a new graph
 var Graph = function() {
-  // create container for nodes. Maybe array?
-  // create container for edges. Object? 
-  // we can use then have an array of nodes to see which ones are connected
-  /* Example: 
-        nodes = [1,2,3,4,5,6];  // might be a problem with duplicate node values
-        edgesObj = {node[5]: node[1],   
-                    node[2]: node[6]],
-                    node[3]: node[4]}  
-                    // Do we care about nodes with more than one edge? if so we might want to do something like below
-                    node[5]: {connectedTo: [node[1], node[2]]};
-        };
-        */
+  this.graph = {};
+
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  // push node into our nodes array
+  // push to nodes
+  this.graph[node] = [];
+
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  // see if node value is included in node array, and return index?
+  for (let key in this.graph) {
+
+    if (key === node.toString()) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  // use contains function and remove from array
+  //if we have time, we can sort every edge array and then just find it by charAt
+  let nodeToDelete = this.graph[node];
+  delete this.graph[node];
+  for (let key in this.graph) {
+    for (let i = 0; i < this.graph[key]; i++) {
+      if (this.graph[key][i] === node) {
+        this.graph[key].splice(i, 1);
+      }
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  // traverse through edges object to see if nodes are connected(or in our case if they are in the same inner array)
+  //for fromnode, we check it tonode is in the array
+  for (let i = 0; i < this.graph[fromNode].length; i++) {
+    if (this.graph[fromNode][i] === toNode) {
+      return true;
+    }
+
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  // use contains to find 1st node and add an edge property to our edge object
+
+  //node={4 : [1,2,3]}
+  //this.graph[node] = {edges: []};
+  this.graph[fromNode].push(toNode);
+  this.graph[toNode].push(fromNode);
+
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  // traverse through edges object and delete where fromNode is contained
+  for (let i = 0; i < this.graph[fromNode].length; i++) {
+    if (this.graph[fromNode][i] === toNode) {
+      this.graph[fromNode].splice(i, 1 );
+    }
+  }
+  for (let i = 0; i < this.graph[toNode].length; i++) {
+    if (this.graph[toNode][i] === fromNode) {
+      this.graph[toNode].splice(i, 1 );
+    }
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  // No clue how to do this, maybe use edges to go through nodes?
+  for ( let key in this.graph) {
+    cb(key);
+  }
 };
-
-var newGraph = new Graph();
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
