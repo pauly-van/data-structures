0a152c220abd3baf951548ede2d7bd5ade9fef58
Paul Nguyen <paulngu1120@gmail.com>
2020-09-11 12:26:01 +0000

    up to hashtables need to figure out collision

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,27 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index]) { 
+    let linkedList = this._storage[index];
+    this._storage[index] = [linkedList].push(v);
+  }
+  this._storage[index] = v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (Array.isArray(this._storage[index])) { // need to retrieve elements in linkedlist
+    console.log('is array is working');
+  }
+  return this._storage[index];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage[index];
 };
 
-
+var newTable = new HashTable();
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = 1;
 };
 
 setPrototype.contains = function(item) {
+  for (let key in this._storage) {
+    if (key === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if ( this.contains(item) ) {
+    delete this._storage[item];
+  }
 };
 
 let freshSet = new Set();
